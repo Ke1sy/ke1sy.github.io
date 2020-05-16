cc89e2ae887cbc927f1937398aff699466b52777
Alina Andreeva <andreeva.a.wezom@gmail.com>
2020-05-16 15:22:25 +0300

    birthdays

---
+++ bithdays/precache-manifest.e3ed57d5a6512e3913ea2a7af855caed.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9340443f5420ed7712fc4cf64eb2347",
+    "url": "/index.html"
+  },
+  {
+    "revision": "918679ea596dd4b9289f",
+    "url": "/static/css/main.c50037b6.chunk.css"
+  },
+  {
+    "revision": "59627b782ac8a9aeaa7e",
+    "url": "/static/js/2.73c1dc08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.73c1dc08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "918679ea596dd4b9289f",
+    "url": "/static/js/main.4cb15305.chunk.js"
+  },
+  {
+    "revision": "4d43d3f9fe58f4aeb28c",
+    "url": "/static/js/runtime-main.0d8c918c.js"
+  },
+  {
+    "revision": "2f35c6a7e0d4c96bdc1d41ee3efc560f",
+    "url": "/static/media/VodafoneRg-Bold.2f35c6a7.woff"
+  },
+  {
+    "revision": "a05934ffebfd6138dc955cdcd4923673",
+    "url": "/static/media/VodafoneRg-Bold.a05934ff.woff2"
+  },
+  {
+    "revision": "49a8cef5fd7876712f328f71a745fcd2",
+    "url": "/static/media/VodafoneRg-Regular.49a8cef5.woff2"
+  },
+  {
+    "revision": "dc7364a508cc18dee854cc3261c868e6",
+    "url": "/static/media/VodafoneRg-Regular.dc7364a5.woff"
+  },
+  {
+    "revision": "eb354e4eb8e179836b4323c399102a5e",
+    "url": "/static/media/arrow-right.eb354e4e.svg"
+  }
+]);
